f4f09c5d4ec7d2912f104e9ef3112768b334acd3
KewinLizarraga <kewin392@gmail.com>
2020-10-30 14:30:03 -0500

    Route Pagination added

--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -3,9 +3,10 @@ const route = require("express").Router();
 const { bookControllers } = require("../controllers");
 
 route.get("/", bookControllers.listAll);
-route.get('/:_id', bookControllers.listOne)
+route.get("/:_id", bookControllers.listOne);
+route.get("/pag/:page", bookControllers.pagination);
 route.post("/", bookControllers.create);
-route.put('/:_id', bookControllers.update)
-route.delete('/:_id', bookControllers.delete)
+route.put("/:_id", bookControllers.update);
+route.delete("/:_id", bookControllers.delete);
 
 module.exports = route;
