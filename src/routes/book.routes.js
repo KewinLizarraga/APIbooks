141e38043078818a9bc53c1b7a204df3900910a8
KewinLizarraga <kewin392@gmail.com>
2020-11-14 01:21:55 -0500

    [UPDATE] route listByPage

--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -3,8 +3,8 @@ const route = require("express").Router();
 const { bookControllers } = require("../controllers");
 
 route.get("/", bookControllers.listAll);
+route.get("/pag/:page", bookControllers.listByPage);
 route.get("/:_id", bookControllers.listOne);
-route.get("/pag/:page", bookControllers.pagination);
 route.post("/", bookControllers.create);
 route.put("/:_id", bookControllers.update);
 route.delete("/:_id", bookControllers.delete);
