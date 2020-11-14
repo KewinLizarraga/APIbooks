020bc52fbbb6d76b7629415935891c6e12b2247a
KewinLizarraga <kewin392@gmail.com>
2020-11-14 01:20:50 -0500

    [UPDATE] change pagination to listByPage

--- src/controllers/book.controllers.js
+++ src/controllers/book.controllers.js
@@ -1,51 +1,16 @@
-const { Book } = require("../models");
+const { Book } = require('../models');
 
 module.exports = {
-  create: async (req, res) => {
-    try {
-      if (Object.keys(req.body).length === 0) {
-        return res.status(200).json({ message: "No data." });
-      }
-      const oldBook = await Book.findOne({ isbn: req.body.isbn });
-      if (oldBook) {
-        return res
-          .status(200)
-          .json({ message: "Book is duplicated, enter another." });
-      }
-      const book = new Book(req.body);
-      book.save();
-      res.status(201).json({ status: 201, message: "Registered book." });
-    } catch (error) {
-      console.log("ERROR", error);
-      res.status(500).json({ message: "Internal Server Error." });
-    }
-  },
   listAll: async (req, res) => {
     try {
-      const booksCount = await Book.countDocuments();
       const books = await Book.find();
-      if (!books) {
-        return res.status(200).json({ message: "No books registered." });
-      }
-      res.status(200).json({ booksCount, books });
+      res.status(200).json(books);
     } catch (error) {
-      console.log("ERROR", error);
-      res.status(500).json({ message: "Internal Server Error." });
+      console.log('ERROR', error);
+      res.status(500).json({ message: 'Internal Server Error.' });
     }
   },
-  listOne: async (req, res) => {
-    try {
-      const book = await Book.findById(req.params._id);
-      if (!book) {
-        return res.status(200).json({ message: "The book does not exist." });
-      }
-      res.status(200).json(book);
-    } catch (error) {
-      console.log("ERROR", error);
-      res.status(500).json({ message: "Internal Server Error." });
-    }
-  },
-  pagination: async (req, res) => {
+  listByPage: async (req, res) => {
     try {
       let perPage = 10;
       let page = req.params.page || 1;
@@ -57,12 +22,39 @@ module.exports = {
       const countBooksPage = books.length;
       const current = page;
       const pages = Math.ceil(countBooks / perPage);
-      res
-        .status(200)
-        .json({ countBooks, perPage, pages, current, countBooksPage, books });
+      res.status(200).json({ countBooks, perPage, pages, current, countBooksPage, books });
     } catch (error) {
-      console.log("ERROR", error);
-      res.status(500).json({ message: "Internal Server Error." });
+      console.log('ERROR', error);
+      res.status(500).json({ message: 'Internal Server Error.' });
+    }
+  },
+  listOne: async (req, res) => {
+    try {
+      const book = await Book.findById(req.params._id);
+      if (!book) {
+        return res.status(200).json({ message: 'The book does not exist.' });
+      }
+      res.status(200).json(book);
+    } catch (error) {
+      console.log('ERROR', error);
+      res.status(500).json({ message: 'Internal Server Error.' });
+    }
+  },
+  create: async (req, res) => {
+    try {
+      if (Object.keys(req.body).length === 0) {
+        return res.status(200).json({ message: 'No data.' });
+      }
+      const oldBook = await Book.findOne({ isbn: req.body.isbn });
+      if (oldBook) {
+        return res.status(200).json({ message: 'Book is duplicated, enter another.' });
+      }
+      const book = new Book(req.body);
+      book.save();
+      res.status(201).json({ status: 201, message: 'Registered book.' });
+    } catch (error) {
+      console.log('ERROR', error);
+      res.status(500).json({ message: 'Internal Server Error.' });
     }
   },
   update: async (req, res) => {
@@ -70,34 +62,35 @@ module.exports = {
       const book = await Book.findById(req.params._id);
       if (!book) {
         return res.status(200).json({
-          message: "Cannot be edited because the book does not exist.",
+          message: 'Cannot be edited because the book does not exist.'
         });
       }
       const oldBook = await Book.findOne({ isbn: req.body.isbn });
       if (oldBook) {
         return res.status(200).json({
-          message:
-            "Book is duplicated, enter another. Check ISBN or remove it to update",
+          message: 'Book is duplicated, enter another. Check ISBN or remove it to update'
         });
       }
       await Book.findByIdAndUpdate(book._id, req.body);
-      res.status(200).json({ message: "Updated book." });
-    } catch (error) {}
-    console.log("ERROR", error);
+      res.status(200).json({ message: 'Updated book.' });
+    } catch (error) {
+      console.log('ERROR', error);
+      res.status(500).json({ message: 'Internal Server Error.' });
+    }
   },
   delete: async (req, res) => {
     try {
       const book = await Book.findById(req.params._id);
       if (!book) {
         return res.status(200).json({
-          message: "Can't be deleted, because the book doesn't exist.",
+          message: "Can't be deleted, because the book doesn't exist."
         });
       }
       await Book.findByIdAndDelete(book._id);
-      res.status(200).json({ message: "Deleted book." });
+      res.status(200).json({ message: 'Deleted book.' });
     } catch (error) {
-      console.log("ERROR", error);
-      res.status(500).json({ message: "Internal Server Error." });
+      console.log('ERROR', error);
+      res.status(500).json({ message: 'Internal Server Error.' });
     }
-  },
+  }
 };
