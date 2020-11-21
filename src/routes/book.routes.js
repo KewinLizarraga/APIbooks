c077720063e09a3c658bfa3a7faddf7f235cbf88
KewinLizarraga <kewin392@gmail.com>
2020-11-21 14:04:04 -0500

    [UPDATE] Protected routes

--- src/routes/book.routes.js
+++ src/routes/book.routes.js
@@ -1,12 +1,13 @@
-const route = require("express").Router();
+const route = require('express').Router();
 
-const { bookControllers } = require("../controllers");
+const { Auth } = require('../middlewares');
+const { bookControllers } = require('../controllers');
 
-route.get("/", bookControllers.listAll);
-route.get("/pag/:page", bookControllers.listByPage);
-route.get("/:_id", bookControllers.listOne);
-route.post("/", bookControllers.create);
-route.put("/:_id", bookControllers.update);
-route.delete("/:_id", bookControllers.delete);
+route.get('/', bookControllers.listAll);
+route.get('/pag/:page', bookControllers.listByPage);
+route.get('/:_id', bookControllers.listOne);
+route.post('/', Auth.authenticate, bookControllers.create);
+route.put('/:_id', Auth.authenticate, bookControllers.update);
+route.delete('/:_id', Auth.authenticate, bookControllers.delete);
 
 module.exports = route;
