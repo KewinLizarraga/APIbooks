68d17aefa47d0ae642eb1d96db60d5d1869b6456
KewinLizarraga <kewin392@gmail.com>
2020-11-21 13:58:22 -0500

    [UPDATE] Bcrypt hash and comparePassword method added

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,9 +1,33 @@
+const bcrypt = require('bcrypt');
 const { Schema, model } = require('mongoose');
 
 const UserSchema = new Schema({
-  username: String,
-  email: String,
-  password: String
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 });
+
+UserSchema.pre('save', function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  this.password = bcrypt.hashSync(this.password, 10);
+  next();
+});
+
+UserSchema.methods.comparePassword = function (password) {
+  return bcrypt.compareSync(password, this.password);
+};
 
 module.exports = model('user', UserSchema);
